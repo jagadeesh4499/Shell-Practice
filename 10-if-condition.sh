59252c054d152ddeb0eba7889d0ec38fa481874f
jagadeesh <jagadeeshakula123@gmail.com>
2024-08-27 20:57:57 +0530

    10-if-condition.sh committed

--- 10-if-condition.sh
+++ 10-if-condition.sh
@@ -1,11 +1,23 @@
+# #!/bin/bash
+
+# NUMBER1=$1
+# NUMBER2=$2
+
+# if[$NUMBER1 -gt $NUMBER2]
+# then
+#     echo "$NUMBER1 is greater than $NUMBER2"
+# else
+#     echo "$NUMBER1 is less than $NUMBER2"
+# fi
+
+
 #!/bin/bash
 
-NUMBER1=$1
-NUMBER2=$2
+NUMBER=$1
 
-if[$NUMBER1 -gt $NUMBER2]
+if [ $NUMBER -gt 20 ] #gt, lt, eq, -ne, -ge, -le
 then
-    echo "$NUMBER1 is greater than $NUMBER2"
+    echo "Given number: $NUMBER is greater than 20"
 else
-    echo "$NUMBER1 is less than $NUMBER2"
+    echo "Given number: $NUMBER is less than 20"
 fi
