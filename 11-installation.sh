89174f0a11b1758efa9a9a2b80a12598fd042d3c
jagadeesh <jagadeeshakula123@gmail.com>
2024-08-27 22:36:13 +0530

    11-installation.sh committed

--- 11-installation.sh
+++ 11-installation.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 #Checking Root Preveleges
 USERID=$(id -u)
-if [$USERID -ne 0]
+if [ $USERID -ne 0 ] 
 then 
     echo "Please run this script with root preveleges"
     exit 1
 fi
 #Installing Tree
 dnf list installed tree
-if [$? -ne 0]
+if [ $? -ne 0 ]
 then
     echo "Tree is not installed....going to install it"
     dnf install tree -y
-    if [$? -ne 0]
+    if [ $? -ne 0 ]
     then
         echo "Tree installation not sucess"
         exit 1
