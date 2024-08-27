5ec42e511425017f107f19f6e95fe68c8d3d5692
jagadeesh <jagadeeshakula123@gmail.com>
2024-08-27 20:14:44 +0530

    06-array.sh committed

--- 06-array.sh
+++ 06-array.sh
@@ -4,4 +4,4 @@ FRUITS=("APPLE" "MANGO" "DRAGON FRUIT")
 
 echo "First Fruit is : $FRUITS[0]"
 echo "Second Fruit is : $FRUITS[1]"
-echo "Third Fruit is : $FRUITS[2]
+echo "Third Fruit is : $FRUITS[2]"
