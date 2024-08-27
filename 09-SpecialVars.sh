8eca96e4252cfcb2b81498c993f364e8e3ae6269
jagadeesh <jagadeeshakula123@gmail.com>
2024-08-27 20:35:30 +0530

    09-SpecialVars.sh committed

--- 09-SpecialVars.sh
+++ 09-SpecialVars.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
 echo "Variables passed to the script : $@"
+echo "Number of variables passed to the script : $#"
+echo "To know the name of script : $0"
+echo "To know the current working directory : $PWD"
+echo "To know the home path : $HOME"
+echo "To know the PID of current running shell script : $$"
+sleep 10 &
+echo "To know the PID of last background command : $!"
