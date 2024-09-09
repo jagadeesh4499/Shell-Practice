8284d7881ebf8dcce188d6431652b388d0ef88f6
jagadeesh <jagadeeshakula123@gmail.com>
2024-09-09 20:03:36 +0530

    functions.sh

--- 12-functions.sh
+++ 12-functions.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 USERID=$(id -u)
 CHECK_ROOT(){
-    if [$USERID -ne 0]
+    if [ $USERID -ne 0 ]
     then
         echo "Please run this script with root previleges"
         exit 1
     fi
 }
 VALIDATE(){
-    if [$1 -ne 0]
+    if [ $1 -ne 0 ]
     then
         echo "$2 installation is not success"
         exit 1
@@ -18,7 +18,7 @@ VALIDATE(){
 }
 CHECK_ROOT
 dnf list installed tree
-if [ $? -ne 0]
+if [ $? -ne 0 ]
 then
     echo "tree is not installed.....going to install it"
     dnf install tree -y
