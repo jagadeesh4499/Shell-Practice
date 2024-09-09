8322ac6bafd8fd3020cee31ce1ba5d5fb52d0487
jagadeesh <jagadeeshakula123@gmail.com>
2024-09-09 20:47:55 +0530

    functions.sh

--- 12-functions.sh
+++ 12-functions.sh
@@ -1,30 +1,36 @@
-# #!/bin/bash
-# USERID=$(id -u)
-# CHECK_ROOT(){
-#     if [ $USERID -ne 0 ]
-#     then
-#         echo "Please run this script with root previleges"
-#         exit 1
-#     fi
-# }
-# VALIDATE(){
-#     if [ $1 -ne 0 ]
-#     then
-#         echo "$2 installation is not success"
-#         exit 1
-#     else
-#         echo "$2 installation is succcess"
-#     fi
-# }
-# CHECK_ROOT
-#dnf list installed tree
-# if [ $? -ne 0 ]
-# then
-#     echo "tree is not installed.....going to install it"
-#     dnf install tree -y
-#     VALIDATE $? "tree"
-# else
-#     echo "tree is already installed."
-# fi
-
+#!/bin/bash
+USERID=$(id -u)
+CHECK_ROOT(){
+    if [ $USERID -ne 0 ]
+    then
+        echo "Please run this script with root previleges"
+        exit 1
+    fi
+}
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+        echo "$2 installation is not success"
+        exit 1
+    else
+        echo "$2 installation is succcess"
+    fi
+}
+CHECK_ROOT
+dnf list installed tree
+if [ $? -ne 0 ]
+then
+    echo "tree is not installed.....going to install it"
+    dnf install tree -y
+    VALIDATE $? "tree"
+else
+    echo "tree is already installed."
+fi
 dnf list installed mvn
+if [$? -ne 0 ]
+then 
+    echo "Maven is not installed.....going to install it"
+    dnf install maven -y
+    VALIDATE $? "Maven"
+else
+    echo "Maven is already installed"
