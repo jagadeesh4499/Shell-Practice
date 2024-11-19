fedceb33287545a04ea8349cb3eef61e1f3a1b11
jagadeesh <jagadeeshakula123@gmail.com>
2024-11-19 17:52:52 +0530

    colours file commmitted

--- 16-redirectors.sh
+++ 16-redirectors.sh
@@ -7,6 +7,7 @@ mkdir -p $LOGS_FOLDER
 R="\e[31m"
 G="\e[32m"
 N="\e[0m"
+Y="\e[33m"
 USERID=$(id -u)
 CHECK_ROOT(){
     if [ $USERID -ne 0 ]
@@ -18,22 +19,22 @@ CHECK_ROOT(){
 VALIDATE(){
     if [ $1 -ne 0 ]
     then
-        echo -e "$2 Installation is.....$R FAILED" &>>$LOG_FILE
+        echo -e "$2 Installation is.....$R FAILED $N" &>>$LOG_FILE
         exit 1
     else
-        echo -e "$2 Installation is.....$G SUCCESS" &>>$LOG_FILE
+        echo -e "$2 Installation is.....$G SUCCESS $N" &>>$LOG_FILE
     fi
 }
 CHECK_ROOT
-# for i in $@
-# do
-#     dnf list installed $i
-#     if [ $? -ne 0 ]
-#     then
-#         echo "$i is not installed.....going to install it"
-#         dnf install $i -y
-#         VALIDATE $? "$i"
-#     else
-#         echo "$i is already installed nothing to do"
-#     fi
-# done
+for i in $@
+do
+    dnf list installed $i &>>$LOG_FILE
+    if [ $? -ne 0 ]
+    then
+        echo "$i is not installed.....going to install it" &>>$LOG_FILE
+        dnf install $i -y &>>$LOG_FILE
+        VALIDATE $? "$i"
+    else
+        echo "$i is already $Y installed nothing to do $N" &>>$LOG_FILE
+    fi
+done
