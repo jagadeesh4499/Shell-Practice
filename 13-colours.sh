35ea0519e70036be6daa6e4fc57b6da98b4baeaf
jagadeesh <jagadeeshakula123@gmail.com>
2024-11-19 11:49:21 +0530

    colours file commmitted

---
+++ 13-colours.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+USERID=$(id -u)
+CHECK_ROOT(){
+    if [ $USERID -ne 0 ]
+    then
+        echo "Please run this script with root priveleges"
+        exit 1
+    fi
+}
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+        echo "$2 Installation is.....$R FAILED"
+        exit 1
+    else
+        echo "$2 Installation is.....$G SUCCESS"
+    fi
+}
+CHECK_ROOT
+dnf list installed tree
+if [ $? -ne 0 ]
+then
+    echo "tree is not installed.....going to install it"
+    dnf install tree -y
+    VALIDATE $? "tree"
+else
+    echo "tree is already installed."
+fi
+dnf list installed ansible
+if [ $? -ne 0 ]
+then 
+    echo "ansible is not installed.....going to install it"
+    dnf install ansible -y
+    VALIDATE $? "ansible"
+else
+    echo "ansible is already installed"
+fi
