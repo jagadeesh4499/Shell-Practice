0968030c00c6ebea0e3ee61faa0886199729265d
jagadeesh <jagadeeshakula123@gmail.com>
2024-11-20 22:11:43 +0530

    shell

--- 17-delete-old-logs.sh
+++ 17-delete-old-logs.sh
@@ -10,10 +10,10 @@ else
     echo -e "$SOURCE_DIR....$R doesn't Exists $N"
     exit 1
 fi
-FILES=$(find $SOURCE_DIR -name "*.log" -mtime +14 -exec ll {}\;)
+FILES=$(find $SOURCE_DIR -name "*.log" -mtime +14)
 echo "FILES : $FILES"
-# while IFS= read -r file #IFS,internal field seperatpor, empty it will ignore while space.-r is for not to ingore special charecters like /
-# do
-#     echo "Deleting file : $file"
-#     rm -rf $file
-# done <<< $FILES
+while IFS= read -r file #IFS,internal field seperatpor, empty it will ignore while space.-r is for not to ingore special charecters like /
+do
+    echo "Deleting file : $file"
+    rm -rf $file
+done <<< $FILES
