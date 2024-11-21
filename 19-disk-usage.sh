e7d1d723d1c3ea209b101f37630901ebf3dd6148
jagadeesh <jagadeeshakula123@gmail.com>
2024-11-21 15:35:01 +0530

    shell

--- 19-disk-usage.sh
+++ 19-disk-usage.sh
@@ -4,7 +4,9 @@ DISK_THRESHOLD=10
 while IFS= read -r line #IFS,internal field seperatpor, empty it will ignore while space.-r is for not to ingore special charecters like /
 do
     USAGE=$(echo $line | awk -F " " '{print $6F}' | cut -d "%" -f1)
-    echo "$USAGE"
     PARTITION=$(echo $line | awk -F " " '{print $NF}')
-    echo "$PARTITION"
+    if [ $DISK_USAGE -ge $DISK_THRESHOLD ]
+    then
+        echo "$PARTITION is more than $DISK_THRESHOLD, current value: $USAGE. Please check"
+    fi
 done <<< $DISK_USAGE
