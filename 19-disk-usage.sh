db5c34438213653ae5ca125225e64eb59c6e83df
jagadeesh <jagadeeshakula123@gmail.com>
2024-11-21 15:28:17 +0530

    shell

--- 19-disk-usage.sh
+++ 19-disk-usage.sh
@@ -3,6 +3,6 @@ DISK_USAGE=$(df -hT|grep xfs)
 DISK_THRESHOLD=10
 while IFS= read -r line #IFS,internal field seperatpor, empty it will ignore while space.-r is for not to ingore special charecters like /
 do
-    USAGE=$(echo $line | awk -F " " '{print $6F}')
+    USAGE=$(echo $line | awk -F " " '{print $6F}' | cut -d "%" -f1)
     echo "$USAGE"
 done <<< $DISK_USAGE
